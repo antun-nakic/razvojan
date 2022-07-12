bad7d5f4789feddafec960dee320a05a7a523b30
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-07-12 20:22:37 +0200

    u pauzi, webpack malo bolje pripremljen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import sum from "./sum.js";
+import dodajKomponentu from "./dodajElement.js";
+import "./style.css";
 
 document.getElementById("forma").addEventListener("submit", (event) => {
   event.preventDefault();
   let a = parseInt(document.getElementById("prvi").value);
   let b = parseInt(document.getElementById("drugi").value);
   console.log(sum(a, b));
+  document.body.appendChild(dodajKomponentu());
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -7,10 +8,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: "./src/index.js",
+  entry: { index: "./src/index.js", addElement: "./src/dodajElement.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].js",
+    publicPath: "",
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
